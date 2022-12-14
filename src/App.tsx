c737014a848faa5e39c436bdc2a7b889e25d5872
Iskandar Mirzo <iskandarzz1993@gmail.com>
2022-12-13 22:44:16 -0500

    Set up react, parcel and bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return <div>
+                <h1>This is car pool APP</h1>
+                <p>This is paragraph</p>
+                <button type="button" class="btn btn-primary">Primary</button>
+            </div>
+}
+
+export default App
